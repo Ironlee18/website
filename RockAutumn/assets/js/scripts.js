0416c4d39f3068473fa6a32bcf5ba4fb35901e8d
Ironlee18 <49811979+Ironlee18@users.noreply.github.com>
2020-08-28 17:46:10 +0200

    Add files via upload

--- RockAutumn/assets/js/scripts.js
+++ RockAutumn/assets/js/scripts.js
@@ -1,53 +1,14 @@
-$(document).ready(function(){
-    toggleMobileMenu()
-    animateToSection()
-    testimonialCarousel()
-    masonryLayout()
-})
+import Masonry from "masonry-layout";
 
-function toggleMobileMenu(){
-    $('#mobile-menu-toggler').on(click,function(){
-        $(this).toggleClass('is-active')
-        $(".header-nav").toggleClass("visible")
-        $('html, body').toggleClass('disable')
-        console.log("test2")
-    })
-}
+window.onload = () =>{
+    const grid = document.querySelector(".grid")
 
-function animateToSection(){
-
-    $('.header-nav a').on('click', function(){
-
-        if($(window).width <991){
-            toggleMobileMenu();
-        }
-        var goToSec =$(this).attr("href")
-
-        setTimeout(function(){
-        $('html, body').animate({
-            scrollTop: goToSec.offset().top
-        }, 1000)
-    },1000)
+    const masonry= new Masonry(grid,{
+        itemSelector: ".grid-item",
+        gutter: 10,
+        originLeft: false,
+        originTop: false,
     })
 
-}
-
-function testimonialCarousel(){
-    $('.testimonial-cont').owlCarousel({
-        item: 1,
-        autoplay:true,
-        loop: true,
-        autoplaySpeed: 1000
-    })
-}
-
-function masonryLayout(){
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth:360,
-        layoutMode: 'fitRows',
-        fitRows:{
-            gutter: 10
-        }
-    })
+    masonry.on('layoutComplete', () => console.log("Layout finished"))
 }
